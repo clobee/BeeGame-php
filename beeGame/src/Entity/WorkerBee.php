66eea0d35e868d211e6b3402f41940b53291d0f6
Claudius_XVIII <cbindika@gmail.com>
2020-02-23 20:47:53 +0000

    Adds WorkerBee code #beeGame

--- beeGame/src/Entity/WorkerBee.php
+++ beeGame/src/Entity/WorkerBee.php
@@ -6,5 +6,7 @@ namespace php_exercices\Entity;
 
 final class WorkerBee extends AbstractBee implements InterfaceBee
 {
-
+    protected $lifespan = 75;
+    protected $hitCost = 10;
+    protected $name = "worker";
 }
--- beeGame/tests/Entity/WorkerBeeTest.php
+++ beeGame/tests/Entity/WorkerBeeTest.php
@@ -3,9 +3,91 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
-
 use php_exercices\Entity\WorkerBee;
 
 final class WorkerBeeTest extends TestCase
 {
+    public function test_worker_has_lifespan()
+    {
+        $bee = new WorkerBee();
+
+        $this->assertSame(
+            $bee->getLifespan(),
+            75
+        );
+    }
+
+    /**
+     * @dataProvider provide_lifespan_damage_data()
+     */
+    public function test_worker_dammage_lifespan($data)
+    {
+        $bee = new WorkerBee();
+
+        for($i=0; $i < $data['hit_count']; $i++) {
+            $bee->hit();
+        }
+
+        $this->assertSame(
+            $bee->getLifespan(),
+            $data['result']
+        );
+    }
+
+    public function provide_lifespan_damage_data() {
+        return [[
+            [
+                'lifespan' => 75,
+                'hit_count' => 4,
+                'result' => 35
+            ],
+            [
+                'lifespan' => 75,
+                'hit_count' => 2,
+                'result' => 55
+            ],
+            [
+                'lifespan' => 75,
+                'hit_count' => 1,
+                'result' => 65
+            ]
+        ]];
+    }
+
+    /**
+     * @dataProvider provide_lifespan_negative_damage_data()
+     */
+    public function test_worker_negative_dammage_returns_0_lifespan($data)
+    {
+        $bee = new WorkerBee();
+
+        for($i=0; $i < $data['hit_count']; $i++) {
+            $bee->hit();
+        }
+
+        $this->assertSame(
+            $bee->getLifespan(),
+            $data['result']
+        );
+    }
+
+    public function provide_lifespan_negative_damage_data() {
+        return [[
+            [
+                'lifespan' => 75,
+                'hit_count' => 20,
+                'result' => 0
+            ],
+            [
+                'lifespan' => 75,
+                'hit_count' => 2,
+                'result' => 55
+            ],
+            [
+                'lifespan' => 75,
+                'hit_count' => 100,
+                'result' => 0
+            ]
+        ]];
+    }
 }
